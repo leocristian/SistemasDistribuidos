8a5c1a914a2d9994486b2e1cb4f72995e3a0613e
leocristian <leonardosclopes@gmail.com>
2022-07-28 11:23:41 -0300

    add threads

---
+++ threads/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Threading;
+using System.Diagnostics;
+
+class Program {
+	static void Main() {
+
+        Process curr = Process.GetCurrentProcess();
+ 
+        // Cria a thread Filha - ThreadStart: Método a ser executado pela thread como parâmetro;
+		Thread t1 = new Thread(new ThreadStart(ExecAction));
+		t1.Start();
+
+		Console.WriteLine($"Sou pai ({curr.Id})");
+		
+		Console.ReadKey();
+	}
+	static void ExecAction() {
+
+        Process curr = Process.GetCurrentProcess();
+		Console.WriteLine($"Sou filha de: {curr.Id}");
+
+	}
+}
+
+
